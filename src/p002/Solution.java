477fcd79e9437f128236226e71d3706188f5e416
sejinsjn <sedad_@hotmail.de>
2022-04-21 18:56:30 +0200

    Problem #2 solved.

---
+++ src/p002/Solution.java
@@ -0,0 +1,24 @@
+package p002;
+
+import java.util.*;
+
+public class Solution {
+
+    private final int[] numbers;
+
+    public Solution(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public void solution(){
+        int num = 1;
+        int[] nums = new int[numbers.length];
+        for(int j = 0; j < numbers.length; j++) {
+            for (int i = 0; i < numbers.length; i++)
+                if (j != i) num *= numbers[i];
+            nums[j] = num;
+            num = 1;
+        }
+        System.out.println("Solution: " + Arrays.toString(nums));
+    }
+}
