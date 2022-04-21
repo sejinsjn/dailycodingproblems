a5b14036be6bcd1b9ded892d854ac550c0aacccb
sejinsjn <sedad_@hotmail.de>
2022-04-21 18:57:23 +0200

    Problem #1

--- src/p001/Main.java
+++ src/p001/Main.java
@@ -1,10 +1,10 @@
-package problem001;
+package p001;
 
 public class Main {
 
     public static void main(String[] args) {
         int[] num = {10,15,2,7,5,9,4,16,1,17,18};
-        Numbers numbers = new Numbers(num, 20);
+        Numbers numbers = new Numbers(num, 30);
         numbers.searchAllSummand();
     }
 }
--- src/p001/Numbers.java
+++ src/p001/Numbers.java
@@ -1,4 +1,4 @@
-package problem001;
+package p001;
 
 public class Numbers {
 
