0f4726e4c785ec54709ff8b5b739d1255e50212d
sejinsjn <sedad_@hotmail.de>
2022-04-21 15:33:46 +0200

    Problem001 solved.

---
+++ src/problem001/Numbers.java
@@ -0,0 +1,19 @@
+package problem001;
+
+public class Numbers {
+
+    private final int[] numbers;
+    private final int result;
+
+    public Numbers(int[] numbers, int result) {
+        this.numbers = numbers;
+        this.result = result;
+    }
+
+    public void searchAllSummand(){
+        for (int a : numbers)
+            for(int b : numbers)
+                if((a+b) == result)
+                    System.out.println(a + " + " + b + " = " + result);
+    }
+}
